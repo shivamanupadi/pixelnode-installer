a03201f462d41e69aa6d0fc59eb2e2adb4be4bae
shivamanupadi <137275428+shivamanupadi@users.noreply.github.com>
2023-12-27 07:31:21 +0530

    installer

--- install.sh
+++ install.sh
@@ -186,4 +186,4 @@ docker-compose -f docker-compose.algorand.testnet.yml pull
 docker-compose -f docker-compose.algorand.betanet.yml pull
 
 echo -e "\033[32m\nCongratulations! Your pixelnode instance is ready to use.\n\033[0m"
-echo -e "\033[32m Please visit http://$(curl -4s https://ifconfig.io):8000 to get started. \033[0m"
+echo -e "\033[32m\nPlease visit http://$(curl -4s https://ifconfig.io):8000 to get started.\n\033[0m"
