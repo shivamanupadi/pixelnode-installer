28b73459accb2cf93937dc52ae089cadb11f135f
shivamanupadi <137275428+shivamanupadi@users.noreply.github.com>
2023-12-27 07:05:42 +0530

    bug fix

--- update-manual.sh
+++ update-manual.sh
@@ -2,6 +2,8 @@ pm2 delete pixelnode
 git reset --hard origin/master
 git pull
 yarn install
+
+bash createEnv.sh
 PORT=8000 pm2 start ecosystem.config.js
 
 echo -e "\033[32mPixelNode upgraded successfully. Running on PORT 8000. Open your server in the browser on the port 8000\033[0m"
