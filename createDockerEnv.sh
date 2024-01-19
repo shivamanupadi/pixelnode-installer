7ad4d64dbe275c868bcae837f7157339c4b3e0ae
shivamanupadi <137275428+shivamanupadi@users.noreply.github.com>
2024-01-19 07:43:39 +0530

    update installer

--- createDockerEnv.sh
+++ createDockerEnv.sh
@@ -20,5 +20,25 @@ add_env_variable() {
     fi
 }
 
+calculate_ram() {
+    if [ "$(uname)" == "Darwin" ]; then
+        total_ram_kb=$(sysctl -n hw.memsize)
+        total_ram_kb=$(($total_ram_kb / 1024))
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+        total_ram_kb=$(grep MemTotal /proc/meminfo | awk '{print $2}')
+    else
+        echo "Unsupported operating system while calculating the RAM"
+        exit 1
+    fi
+
+    total_ram_mib=$((total_ram_kb / (1024)))
+    echo "Total RAM: ${total_ram_mib}MiB"
+    ninety_percent_ram=$((total_ram_mib * 90 / 100))
+    echo "90% of Total RAM: ${ninety_percent_ram}MiB"
+
+    add_env_variable GOMEMLIMIT ${ninety_percent_ram}MiB
+}
+
 create_env_file
 add_env_variable GOGC 50
+calculate_ram
